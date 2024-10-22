365af1fb76230a4b00be9cfcaae3b1c0e4618c58
shafek <engahmedshafek1@gmail.com>
2024-10-22 16:37:18 +0300

    last

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,29 +9,27 @@ const NavBar = () => {
 
   const { data, isLoading, isFetching } = useGetCategoriesQuery();
   console.log(data);
-
   return (
-    <div className=" bg-white     shadow-shadowOrBorder  relative z-10 gap-4 shadow-2xl hidden sm:flex items-center justify-center pt-2 pb-[10px] ">
+    <div className="bg-white  px-4  w-full scroll-hidden shadow-shadowOrBorder relative z-10 gap-4 shadow-2xl hidden sm:flex items-center justify-start lg:justify-center pt-2 pb-[10px] overflow-x-auto whitespace-nowrap">
       {isLoading && isFetching ? (
         <>
           <PulseLoader color="#3B4158" size={10} className="text-center" />
         </>
       ) : (
         <>
-          {" "}
           {data?.data.map((cat) => (
             <p
               onClick={() =>
                 router.push("/category/" + cat?.title.toLowerCase())
               }
               key={cat.documentId}
-              className=" capitalize text-[13px] lg:text-[16px] font-medium lg:font-semibold text-primary hover:text-hoverColor transition ease-in-out cursor-pointer"
+              className="capitalize text-[14px] hover:underline lg:text-[16px] font-medium lg:font-semibold text-primary hover:text-hoverColor transition ease-in-out cursor-pointer inline-block"
             >
               {cat?.title.toLowerCase()}
             </p>
           ))}
         </>
-      )}{" "}
+      )}
     </div>
   );
 };
